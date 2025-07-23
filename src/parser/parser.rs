a9a86e72cb2b0b65e4de0cd578106e602a57790a
uff20xd <donut121008@gmail.com>
2025-07-23 15:27:17 +0200

    k

--- src/parser/parser.rs
+++ src/parser/parser.rs
@@ -1,14 +1,51 @@
 use crate::tokenizer::tokens::Token;
 use crate::tokenizer::tokens::TokenType;
 
+#[derive(Clone, Debug)]
 pub enum NodeType {
+    // { Bool | Integer | Float | String | List | Expr }
     Value,
+
+    // { }
     Bool(bool),
+
+    // { }
     Integer(i128), 
+
+    // { }
+    Float,
+
+    // { }
     String(String),
+
+    // { Value+ }
+    List,
+
+    // { MathExpr | Block | BoolExpr | FunctionCall | VarDecleration }
+    Expr,
+
+    // { Value ~ MathOperator ~ Value }
+    MathExpr,
+
+    // { Plus | Minus | Mult | Divide }
+    MathOperator,
+
+    // { Value ~ MathOperator ~ Value }
+    BoolExpr,
+
+    // { EqualTo | LesserThan | GreaterThan | GreaterOrEqualTo | LesserThanOrEqualTo }
+    BoolOperator,
+
+    //
     Block,
+
     Struct,
+
     Function,
+
+    If,
+
+    For,
 }
 
 pub struct Node {
